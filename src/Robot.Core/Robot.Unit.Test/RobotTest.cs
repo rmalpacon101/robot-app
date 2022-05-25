3304d9e6bbb0054320926a7544717d388ac08b2f
Richard Ikegbulam <malpacon101@yahoo.co.uk>
2022-05-25 11:23:14 +0100

    refactored unit test

--- src/Robot.Core/Robot.Unit.Test/RobotTest.cs
+++ src/Robot.Core/Robot.Unit.Test/RobotTest.cs
@@ -1,39 +1,42 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Robots.Core;
 
-namespace Robots.Unit.Test
+namespace Robots.Unit.Test;
+
+[TestClass]
+public class RobotTest
 {
-    [TestClass]
-    public class RobotTest
+    private Robot _robot;
+
+    [TestInitialize]
+    public void Initialize()
     {
-        [DataTestMethod]
-        [DataRow("R", "0:0:E")]
-        [DataRow("RR", "0:0:S")]
-        [DataRow("RRR", "0:0:W")]
-        [DataRow("RRRR", "0:0:N")]
-        [DataRow("RRRRR", "0:0:E")]
-        public void Rotate_Should_RotateRight(string command, string position)
-        {
-            Robot robot = new();
+        _robot = new Robot();
+    }
 
-            string result = robot.Execute(command);
+    [DataTestMethod]
+    [DataRow("L", "0:0:W")]
+    [DataRow("LL", "0:0:S")]
+    [DataRow("LLL", "0:0:E")]
+    [DataRow("LLLL", "0:0:N")]
+    [DataRow("LLLLL", "0:0:W")]
+    public void Rotate_Should_RotateLeft(string command, string position)
+    {
+        var result = _robot.Execute(command);
 
-            Assert.AreEqual(position, result);
-        }
+        Assert.AreEqual(position, result);
+    }
 
-        [DataTestMethod]
-        [DataRow("L", "0:0:W")]
-        [DataRow("LL", "0:0:S")]
-        [DataRow("LLL", "0:0:E")]
-        [DataRow("LLLL", "0:0:N")]
-        [DataRow("LLLLL", "0:0:W")]
-        public void Rotate_Should_RotateLeft(string command, string position)
-        {
-            Robot robot = new();
+    [DataTestMethod]
+    [DataRow("R", "0:0:E")]
+    [DataRow("RR", "0:0:S")]
+    [DataRow("RRR", "0:0:W")]
+    [DataRow("RRRR", "0:0:N")]
+    [DataRow("RRRRR", "0:0:E")]
+    public void Rotate_Should_RotateRight(string command, string position)
+    {
+        var result = _robot.Execute(command);
 
-            string result = robot.Execute(command);
-
-            Assert.AreEqual(position, result);
-        }
+        Assert.AreEqual(position, result);
     }
 }
