eaa27e38c7ff00cb73a1ae5a246c7f746cddf9e2
Richard Ikegbulam <malpacon101@yahoo.co.uk>
2022-05-25 11:20:48 +0100

    refactored direction command

---
+++ src/Robot.Core/Robot.Core/Direction.cs
@@ -0,0 +1,45 @@
+﻿namespace Robots.Core;
+
+public class Direction
+{
+    private readonly string _direction;
+    private readonly string _left;
+    private readonly string _right;
+
+    public Direction(string direction, string right, string left)
+    {
+        _direction = direction;
+        _right = right;
+        _left = left;
+    }
+
+    public static Direction Default => Orientation["N"];
+
+    private static Dictionary<string, Direction> Orientation => new()
+    {
+        ["N"] = new Direction("N", "E", "W"),
+        ["E"] = new Direction("E", "S", "N"),
+        ["S"] = new Direction("S", "W", "E"),
+        ["W"] = new Direction("W", "N", "S")
+    };
+
+    private Direction GetDirection(string newDirection)
+    {
+        return Orientation[newDirection];
+    }
+
+    public string GetValue()
+    {
+        return _direction;
+    }
+
+    public Direction RotateLeft()
+    {
+        return GetDirection(_left);
+    }
+
+    public Direction RotateRight()
+    {
+        return GetDirection(_right);
+    }
+}
--- src/Robot.Core/Robot.Core/Robot.cs
+++ src/Robot.Core/Robot.Core/Robot.cs
@@ -2,48 +2,16 @@
 
 public class Robot
 {
-    private string _direction = "N";
-
     public string Execute(string command)
     {
-        var commands = new Stack<char>(command.ToCharArray());
-
-        while (commands.Count > 0)
-        {
-            char orientation = commands.Pop();
-
-            _direction = orientation switch
+        var direction = command.ToCharArray()
+            .Aggregate(Direction.Default, (current, directionCommand) => directionCommand switch
             {
-                'R' => RotateRight(),
-                'L' => RotateLeft(),
-                _ => _direction
-            };
-        }
+                'R' => current.RotateRight(),
+                'L' => current.RotateLeft(),
+                _ => current
+            });
 
-        return $"0:0:{_direction}";
-    }
-
-    private string RotateLeft()
-    {
-        return _direction switch
-        {
-            "N" => "W",
-            "W" => "S",
-            "S" => "E",
-            "E" => "N",
-            _ => "N"
-        };
-    }
-
-    private string RotateRight()
-    {
-        return _direction switch
-        {
-            "N" => "E",
-            "E" => "S",
-            "S" => "W",
-            "W" => "N",
-            _ => "N"
-        };
+        return $"0:0:{direction.GetValue()}";
     }
 }
