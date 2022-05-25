0a5f14e30ed328fd7ef192b9878a7525c5f26086
Richard Ikegbulam <malpacon101@yahoo.co.uk>
2022-05-25 10:07:41 +0100

    added left, right direction/orientation

---
+++ src/Robot.Core/Robot.Core/Robot.cs
@@ -0,0 +1,49 @@
+﻿namespace Robots.Core;
+
+public class Robot
+{
+    private string _direction = "N";
+
+    public string Execute(string command)
+    {
+        var commands = new Stack<char>(command.ToCharArray());
+
+        while (commands.Count > 0)
+        {
+            char orientation = commands.Pop();
+
+            _direction = orientation switch
+            {
+                'R' => RotateRight(),
+                'L' => RotateLeft(),
+                _ => _direction
+            };
+        }
+
+        return $"0:0:{_direction}";
+    }
+
+    private string RotateLeft()
+    {
+        return _direction switch
+        {
+            "N" => "W",
+            "W" => "S",
+            "S" => "E",
+            "E" => "N",
+            _ => "N"
+        };
+    }
+
+    private string RotateRight()
+    {
+        return _direction switch
+        {
+            "N" => "E",
+            "E" => "S",
+            "S" => "W",
+            "W" => "N",
+            _ => "N"
+        };
+    }
+}
---
+++ src/Robot.Core/Robot.Unit.Test/RobotTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Robots.Core;
+
+namespace Robots.Unit.Test
+{
+    [TestClass]
+    public class RobotTest
+    {
+        [DataTestMethod]
+        [DataRow("R", "0:0:E")]
+        [DataRow("RR", "0:0:S")]
+        [DataRow("RRR", "0:0:W")]
+        [DataRow("RRRR", "0:0:N")]
+        [DataRow("RRRRR", "0:0:E")]
+        public void Rotate_Should_RotateRight(string command, string position)
+        {
+            Robot robot = new();
+
+            string result = robot.Execute(command);
+
+            Assert.AreEqual(position, result);
+        }
+
+        [DataTestMethod]
+        [DataRow("L", "0:0:W")]
+        [DataRow("LL", "0:0:S")]
+        [DataRow("LLL", "0:0:E")]
+        [DataRow("LLLL", "0:0:N")]
+        [DataRow("LLLLL", "0:0:W")]
+        public void Rotate_Should_RotateLeft(string command, string position)
+        {
+            Robot robot = new();
+
+            string result = robot.Execute(command);
+
+            Assert.AreEqual(position, result);
+        }
+    }
+}
